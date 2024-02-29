6f88761eadf222ef451ae28714037c1fc730666a
Polina Kozlova <158488988+Anoremia@users.noreply.github.com>
2024-02-29 19:31:15 +0400

    Практика

---
+++ Practice/Task26/Person.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task26
+{
+    public class Person
+    {
+        public int age = 20;
+        public string name = "Egor";
+    }
+
+}
---
+++ Practice/Task26/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace Task26
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Person egor1 = new Person();
+            
+
+            Console.WriteLine("Имя: " + egor1.name);
+            Console.WriteLine();
+            Console.WriteLine("Возраст: " + egor1.age);
+
+        }
+    }
+}
