3d9c8c87493067613f6cbe121ce3da208b0c090b
Polina Kozlova <158488988+Anoremia@users.noreply.github.com>
2024-02-29 21:03:00 +0400

    Практика

---
+++ Practice/Task28/Person.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task28
+{
+    internal class Person
+    {
+        public int age;
+        public string name;
+        public string fename;
+        public string company;
+
+        public Person() 
+        {
+            age;
+            name;
+            fename;
+            company;
+        }
+        
+        public Person()
+        {
+            age = 18;
+            name = "Egor";
+            fename = "Egorovich";
+            company = "Directum";
+        }
+        public Person() 
+        {
+            age = 18;
+            name = "Egor";
+            Person.fename;
+            Person.company;
+        }
+    }
+}
