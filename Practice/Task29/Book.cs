4149ffafceb4b0616d3eb2527120810fa90dcd46
Polina Kozlova <158488988+Anoremia@users.noreply.github.com>
2024-03-07 20:02:10 +0400

    29

---
+++ Practice/Task29/Book.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task29
+{
+    public class Book
+    {
+        public static int BooksCount = 0;
+
+        public string title;
+        public string autor;
+        public int year;
+        public string genre;
+        public Book(string title, string autor, int year, string genre)
+        {
+            this.title = title; 
+            this.autor = autor;
+            this.year = year;
+            this.genre = genre;
+            BooksCount++;
+        }
+
+    }
+}
---
+++ Practice/Task29/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace Task29
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Book book1 = new Book ("Голодные игры", "С.Коллинз", 2008,"Роман и т.д.");
+            Book book2 = new Book("Преступление и наказание", "Ф.М. Достоевский", 1866, "Роман");
+            Book book3 = new Book("Мальчик в полосатой пижаме", "Д.Бойн", 2006, "Роман");
+
+            Console.WriteLine($"Количество книг: {Book.BooksCount}");
+        }
+    }
+}
