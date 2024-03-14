0630f7075091efa7495556cdc46d75b0d7caf86c
Polina Kozlova <158488988+Anoremia@users.noreply.github.com>
2024-03-14 20:57:42 +0400

    6

---
+++ Homework/Task6/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Task6
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array = new { 1, 2, 3, 4, 5 };
+        }
+    }
+}
---
+++ Homework/Task6/Search.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task6
+{
+    internal class Search
+    {
+        
+        public static LinearSearch() 
+        {
+            return;
+            const LinearSearch = (numbersArray, targetNumber) => 
+            
+            {
+                for (let i = 0; i < numbersArray.length; i++)
+                {
+                    if (numbersArray[i] == targetNumber)
+                    {
+                        return i // Элемент найден
+                    }
+                }
+
+                return -1; // Элемент не найден
+            }
+
+        }
+    }
+}
